e79e14eb17c5fb0689da2c5fea4f2bc38c0cfe16
Tomas Aranda <tomas@devschool.com.ar>
2023-08-03 19:19:30 -0300

    comments

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,20 @@
 import React, { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
-import { useEffect } from 'react';
 
 const Home = () => {
-	const navigate = useNavigate();
 	const { user } = useContext(AuthContext);
 
-	/*
-	asegura que si no hay usuario autenticado, la aplicación redirigirá automáticamente 
-	al usuario a la página de inicio de sesión para que inicie sesión antes de acceder a 
-	rutas privadas o protegidas.
-	*/
-	useEffect(() => {
-		if (!user) {
-			return navigate('/login');
-		}
-	}, []);
-
-	/*
-	ste componente muestra un mensaje de bienvenida a la página de inicio (home) con el 
-	nombre y apellido del usuario autenticado si hay un usuario autenticado. Si no hay 
-	usuario autenticado, el componente no muestra nada en la interfaz.
-	*/
-	return user ? (
+	return (
 		<>
-			<div className="mx-2">
+			<div className="mx-2 d-flex  flex-column align-items-center">
 				<h3>
 					Home - Bienvenido{' '}
 					<span className="text-primary">{user.firstname + ' ' + user.lastname}</span>
 				</h3>
+				<button className="btn btn-danger w-50 mt-3">Cerrar Sesion</button>
 			</div>
 		</>
-	) : null;
+	);
 };
 
 export default Home;
